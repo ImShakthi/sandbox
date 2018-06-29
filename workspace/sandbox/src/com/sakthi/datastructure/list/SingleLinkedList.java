f0e20fc2b354bec6d504bc684a8908cf7d258e15
Sakthivel <sakthi836800@gmail.com>
2018-06-30 00:23:27 +0530

    find the Nth element in the given list from end.

--- workspace/sandbox/src/com/sakthi/datastructure/list/SingleLinkedList.java
+++ workspace/sandbox/src/com/sakthi/datastructure/list/SingleLinkedList.java
@@ -1,41 +1,33 @@
 package com.sakthi.datastructure.list;
 
-class SingleLinkedList
-{
+class SingleLinkedList {
 	private SingleNode head = null;
-	
-	public SingleLinkedList()
-	{
-		
+
+	public SingleLinkedList() {
+
 	}
-	
-	public SingleLinkedList(final SingleNode headArg)
-	{
+
+	public SingleLinkedList(final SingleNode headArg) {
 		this.head = headArg;
 	}
-	
-	public int size()
-	{
+
+	public int size() {
 		int size = 0;
 		SingleNode temp = this.head;
-		while (temp != null)
-		{
+		while (temp != null) {
 			size++;
 			temp = temp.getNext();
 		}
 		return size;
 	}
-	
-	public int get(final int index) throws Exception
-	{
+
+	public int get(final int index) throws Exception {
 		int data = 0;
 		int position = 0;
 		boolean indexFound = false;
 		SingleNode temp = this.head;
-		while (temp != null && position <= index)
-		{
-			if (position == index)
-			{
+		while (temp != null && position <= index) {
+			if (position == index) {
 				data = temp.getData();
 				indexFound = true;
 				break;
@@ -43,104 +35,82 @@ class SingleLinkedList
 			position++;
 			temp = temp.getNext();
 		}
-		if (!indexFound)
-		{
+		if (!indexFound) {
 			throw new Exception("Index out of bound.");
 		}
 		return data;
 	}
-	
-	public boolean add(final int data)
-	{
-		try
-		{
-			if (this.head == null)
-			{
+
+	public boolean add(final int data) {
+		try {
+			if (this.head == null) {
 				this.head = new SingleNode();
 				this.head.setData(data);
 				this.head.setNext(null);
 				return true;
-			}
-			else
-			{
+			} else {
 				SingleNode temp = head;
-				while (temp.getNext() != null)
-				{
+				while (temp.getNext() != null) {
 					temp = temp.getNext();
 				}
 				SingleNode newNode = new SingleNode();
 				newNode.setData(data);
 				newNode.setNext(null);
-				
+
 				temp.setNext(newNode);
 				return true;
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 		}
 		return false;
 	}
-	
-	public boolean add(final int position, final int data)
-	{
+
+	public boolean add(final int position, final int data) {
 		int local_position = 0;
-		if (position < 0)
-		{
+		if (position < 0) {
 			System.err.println("Invalid position " + position);
 			return false;
-		}
-		else
-		{
+		} else {
 			boolean validPosition = false;
-			
+
 			SingleNode newNode = new SingleNode();
 			newNode.setData(data);
-			
+
 			SingleNode temp = this.head;
-			if (position == 0)
-			{
+			if (position == 0) {
 				validPosition = true;
 				newNode.setNext(temp);
 				this.head = newNode;
-			}
-			else
-			{
+			} else {
 				SingleNode prev = null;
-				while (temp != null)
-				{
+				while (temp != null) {
 					prev = temp;
 					temp = temp.getNext();
 					local_position++;
-					
-					if (local_position == position)
-					{
+
+					if (local_position == position) {
 						validPosition = true;
 						break;
 					}
 				}
-				if (validPosition)
-				{
+				if (validPosition) {
 					prev.setNext(newNode);
 					newNode.setNext(temp);
 				}
 			}
-			
-			if (!validPosition)
-			{
+
+			if (!validPosition) {
 				System.out.println("Index is ot of bond - " + position);
 			}
 		}
 		return true;
 	}
 
-	public void removeAll()
-	{
+	public void removeAll() {
 		SingleNode node = this.head;
 
-		while(node != null)
-		{
+		while (node != null) {
 			System.out.println(node.getData());
 			SingleNode temp = node;
 			temp = null;
@@ -149,117 +119,125 @@ class SingleLinkedList
 		this.head = null;
 	}
 
-	public void remove(final int data)
-	{
+	public void remove(final int data) {
 		boolean elementFound = false;
 		SingleNode prev = this.head;
 		SingleNode temp = this.head;
-		while (temp != null)
-		{
-			if (temp.getData() == data)
-			{
+		while (temp != null) {
+			if (temp.getData() == data) {
 				elementFound = true;
 				break;
 			}
 			prev = temp;
 			temp = temp.getNext();
 		}
-		if (elementFound)
-		{
-			if (this.head != temp)
-			{
+		if (elementFound) {
+			if (this.head != temp) {
 				prev.setNext(temp.getNext());
 				// Free the memory space
 				temp = null;
-			}
-			else
-			{
-				if (temp.getNext() == null)
-				{
+			} else {
+				if (temp.getNext() == null) {
 					this.head = null;
-				}
-				else
-				{
+				} else {
 					this.head = temp.getNext();
 					temp = null;
 				}
 			}
 			System.out.println("Data " + data + " is removed.");
-		}
-		else
-		{
+		} else {
 			System.out.println("Data " + data + " is not present in the list.");
 		}
 	}
-	
-	public SingleNode find(final int data)
-	{
+
+	public SingleNode find(final int data) {
 		SingleNode temp = this.head;
-		while (temp != null)
-		{
-			if (temp.getData() == data)
-			{
+		while (temp != null) {
+			if (temp.getData() == data) {
 				return temp;
 			}
 			temp = temp.getNext();
 		}
 		return null;
 	}
-	
-	public String toString()
-	{
+
+	public Integer findAtN(final int position) {
+		Integer nodeData = null;
+		try {
+			SingleNode temp_node = head;
+			int index = 0;
+			while (temp_node != null) {
+				temp_node = temp_node.getNext();
+				index++;
+			}
+
+			if (position <= index) {
+				temp_node = head;
+				int stopper = index - position;
+				index = 0;
+				do {
+					if (temp_node != null) {
+						if (stopper == index) {
+							nodeData = temp_node.getData();
+						}
+					} else {
+						throw new Exception("ot of range.");
+					}
+					index++;
+					temp_node = temp_node.getNext();
+				} while (temp_node != null);
+			} else {
+				throw new Exception("Fewer nos of nodes in the list.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return nodeData;
+	}
+
+	public String toString() {
 		String displayStr = "";
-		if (this.head != null)
-		{
+		if (this.head != null) {
 			SingleNode temp = head;
-			while (temp != null)
-			{
+			while (temp != null) {
 				displayStr += temp.getData() + " ";
 				temp = temp.getNext();
 			}
-		}
-		else
-		{
+		} else {
 			displayStr = "List is empty.";
 		}
 		return displayStr;
 	}
 }
 
-class SingleNode
-{
+class SingleNode {
 	private int data;
 	private SingleNode next;
-	
-	public int getData()
-	{
+
+	public int getData() {
 		return data;
 	}
-	
-	public void setData(final int data)
-	{
+
+	public void setData(final int data) {
 		this.data = data;
 	}
-	
-	public SingleNode getNext()
-	{
+
+	public SingleNode getNext() {
 		return next;
 	}
-	
-	public void setNext(final SingleNode next)
-	{
+
+	public void setNext(final SingleNode next) {
 		this.next = next;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "Node [data=" + data + ", next=" + next + "]";
 	}
-	
+
 }
--- workspace/sandbox/src/com/sakthi/datastructure/list/SingleLinkedListHandler.java
+++ workspace/sandbox/src/com/sakthi/datastructure/list/SingleLinkedListHandler.java
@@ -2,52 +2,38 @@ package com.sakthi.datastructure.list;
 
 import java.util.Scanner;
 
-public class SingleLinkedListHandler
-{
-	
-	public static void main(String[] args)
-	{
+public class SingleLinkedListHandler {
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SingleLinkedListHandler singleLinkedList = new SingleLinkedListHandler();
-		singleLinkedList.init();
+		SingleLinkedList list = singleLinkedList.init();
+		System.out.println(list.findAtN(9));
 	}
+
 	
-	public void init()
-	{
+
+	public SingleLinkedList init() {
+		SingleLinkedList list = null;
 		Scanner scanner = null;
-		try
-		{
+		try {
 			scanner = new Scanner(System.in);
-			SingleLinkedList list = new SingleLinkedList();
+			list = new SingleLinkedList();
 			System.out.println(list.toString());
 			list.add(10);
 			list.add(11);
 			list.add(12);
 			list.add(13);
-			list.remove(13);
 			list.add(0, 1);
 			list.add(2, 5);
-			list.add(5, 1810);
-			list.removeAll();
 			System.out.println(list.toString());
-			
-			int len = list.size();
-			for (int index = 0; index < len; index++)
-			{
-				System.out.println(list.get(index));
-			}
-			
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
-		}
-		finally
-		{
-			if (scanner != null)
-			{
+		} finally {
+			if (scanner != null) {
 				scanner.close();
 			}
 		}
+		return list;
 	}
 }
